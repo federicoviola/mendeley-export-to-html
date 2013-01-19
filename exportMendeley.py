03bb5463c31dea7f08b778be4dcd52ffbcd52ede
ljvillanueva <ljvillanueva@coquipr.com>
2013-01-19 16:02:31 -0500

    Working version, with PDFs, doi and alphabetical ordering. Problems with encoding.

--- exportMendeley.py
+++ exportMendeley.py
@@ -10,12 +10,13 @@ Script to get papers from a particular folder in the Desktop version
 #########################################################################
 
 # Import modules
-import commands
+import shutil
 import os
-import sys
 import getpass
 import sqlite3
 import time
+import urllib
+import sys
 
 #########################################################################
 # TEST FOR CONFIG FILE AND FINDING THE DATABASE				#
@@ -33,6 +34,7 @@ linux_username=getpass.getuser()
 conn = sqlite3.connect('/home/' + linux_username + '/.local/share/data/Mendeley Ltd./Mendeley Desktop/' + mendeley_user + '@www.mendeley.com.sqlite')
 #TEMP DB FOR TESTING
 #conn = sqlite3.connect(mendeley_user + '@www.mendeley.com.sqlite')
+conn.text_factory = str
 
 #########################################################################
 # FUNCTION DECLARATIONS							#
@@ -56,7 +58,10 @@ def export(folderID):
 	if folderID == "":
 		print "Error: The Folder ID: " + str(folderID) + " does not exists. \n\n Exiting...\n\n"
 		sys.exit (1)
+	if os.path.exists('html')==True:
+		shutil.rmtree('html')
 	os.makedirs('html')
+	os.makedirs('html/files')
 	cursor = conn.cursor()
 	cursor.execute('SELECT count() FROM Documents, DocumentFolders WHERE DocumentFolders.folderID=' + str(folderID) + ' AND DocumentFolders.documentid=Documents.id AND Documents.type="JournalArticle"')
 	count_files=cursor.fetchone()[0]
@@ -72,11 +77,11 @@ def export(folderID):
 		a_counter=1
 		for y in allauthors:
 			if type(y[0])!=type(None):
-				f_name = y[0].encode('UTF-8')
+				f_name = y[0]
 			else:
 				f_name = ""
 			if type(y[1])!=type(None):
-				l_name = y[1].encode('UTF-8')
+				l_name = y[1]
 			else:
 				l_name = ""
 			if a_counter==1:
@@ -88,23 +93,41 @@ def export(folderID):
 			a_counter = a_counter+1
 		d_year = str(x[1])
 		if type(x[2])!=type(None):
-			d_title = x[2].encode('UTF-8')
+			d_title = x[2]
 		else:
 			d_title = ""
 
 		if type(x[3])!=type(None):
-			d_journal = x[3].encode('UTF-8')
+			d_journal = x[3]
 		else:
 			d_journal = ""
 		if type(x[4])!=type(None):
-			d_pages = x[4].encode('UTF-8')
+			d_pages = x[4]
 		else:
 			d_pages = ""
 		f.write(". " + d_year + ". " + d_title + ". " + d_journal + ": " + d_pages)
+		f.write(". ")
 		if type(x[5])!=type(None):
-			d_doi = x[5].encode('UTF-8')
+			d_doi = x[5]
 			f.write(" doi: <a href=\"http://dx.doi.org/" + d_doi + "\">" + d_doi + "</a>")
-		f.write(".<br>\n")
+		cursor2.close()
+		cursor3 = conn.cursor()
+		cursor3.execute('SELECT count() FROM DocumentFiles, Files WHERE DocumentFiles.documentid=' + str(x[0]) + ' AND DocumentFiles.hash=Files.hash')
+		count_authors=cursor3.fetchone()[0]
+		cursor3.execute('SELECT Files.localUrl FROM DocumentFiles, Files WHERE DocumentFiles.documentid=' + str(x[0]) + ' AND DocumentFiles.hash=Files.hash')
+		allfiles=cursor3.fetchall()
+		for z in allfiles:
+			f_url = z[0]
+			f_url = f_url.encode('ascii')
+			f_url = f_url[7:]
+			if f_url[-3:]=="pdf" or f_url[-3:]=="PDF":
+				f_url = urllib.unquote(f_url)
+				f_url.replace(' ', '\\ ')
+				if os.path.exists(urllib.unquote(f_url))==True:
+					shutil.copy(urllib.unquote(f_url), 'html/files/' + str(x[0]) + '.pdf')
+					f.write(' [<a href=\"files/' + str(x[0]) + '.pdf\">PDF</a>]')
+		cursor3.close()
+		f.write("<br>\n")
 	cursor.close()
 	f.close()
 	f = open('html/index_unordered.html', 'r')
